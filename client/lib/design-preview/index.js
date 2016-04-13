cd913a3af042b83da2ff627d232fbb7a5b087549
Payton Swick <payton@foolord.com>
2016-04-13 15:30:43 -0400

    DesignPreview Lib: Add updater for siteLogo

--- client/lib/design-preview/index.js
+++ client/lib/design-preview/index.js
@@ -1,11 +1,13 @@
 import siteTitle from './updaters/site-title';
 import headerImage from './updaters/header-image';
 import homePage from './updaters/home-page';
+import siteLogo from './updaters/site-logo';
 
 const updaterFunctions = [
 	siteTitle,
 	headerImage,
 	homePage,
+	siteLogo,
 ];
 
 export function updatePreviewWithChanges( previewDocument, customizations, reloadPreview ) {
