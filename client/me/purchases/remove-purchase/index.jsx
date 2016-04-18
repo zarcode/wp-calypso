8f16eb4026021a9645b0bb57c4c9c8876c97dc0e
Derek Springer <derekspringer@gmail.com>
2016-04-18 13:58:40 -0700

    First commit. Just enough to let me get PR started.
    Skeleton of remove purchase survey.

--- client/me/purchases/remove-purchase/index.jsx
+++ client/me/purchases/remove-purchase/index.jsx
@@ -3,10 +3,12 @@
  */
 import page from 'page';
 import React from 'react';
+import shuffle from 'lodash/shuffle';
 
 /**
  * Internal dependencies
  */
+import config from 'config';
 import CompactCard from 'components/card/compact';
 import Dialog from 'components/dialog';
 import { getIncludedDomain, getName, hasIncludedDomain, isRemovable } from 'lib/purchases';
@@ -16,6 +18,19 @@ import { isDomainRegistration, isPlan } from 'lib/products-values';
 import notices from 'notices';
 import purchasePaths from '../paths';
 import { removePurchase } from 'lib/upgrades/actions';
+import FormSectionHeading from 'components/forms/form-section-heading';
+import FormFieldset from 'components/forms/form-fieldset';
+import FormLegend from 'components/forms/form-legend';
+import FormLabel from 'components/forms/form-label';
+import FormRadio from 'components/forms/form-radio';
+import FormTextInput from 'components/forms/form-text-input';
+import FormTextarea from 'components/forms/form-textarea';
+
+/**
+ * Module dependencies
+ */
+import debugFactory from 'debug';
+const debug = debugFactory( 'calypso:purchases:survey' );
 
 const RemovePurchase = React.createClass( {
 	propTypes: {
@@ -29,9 +44,21 @@ const RemovePurchase = React.createClass( {
 	},
 
 	getInitialState() {
+		// shuffle reason order, but keep another_reason last
+		let reasonOrder = shuffle( [
+			'no_use',
+			'not_sure_includes',
+			'different_expected',
+			'want_delete'
+		] );
+		reasonOrder.push( 'another_reason' );
+
 		return {
 			isDialogVisible: false,
-			isRemoving: false
+			isRemoving: false,
+			isAnotherReason: false,
+			checkedRadio: null,
+			reasonOrder: reasonOrder
 		};
 	},
 
@@ -50,6 +77,8 @@ const RemovePurchase = React.createClass( {
 
 		const purchase = getPurchase( this.props ),
 			{ selectedSite, user } = this.props;
+
+		// Tracks update goes here
 
 		removePurchase( purchase.id, user.ID, success => {
 			if ( success ) {
@@ -80,6 +109,13 @@ const RemovePurchase = React.createClass( {
 
 				notices.error( this.props.selectedPurchase.error );
 			}
+		} );
+	},
+
+	handleRadio( event ) {
+		this.setState( {
+			checkedRadio: event.currentTarget.value,
+			isAnotherReason: 'another_reason' === event.currentTarget.name
 		} );
 	},
 
@@ -117,7 +153,8 @@ const RemovePurchase = React.createClass( {
 				className="remove-purchase__dialog"
 				isVisible={ this.state.isDialogVisible }
 				onClose={ this.closeDialog }>
-				<h1>{ this.translate( 'Remove %(productName)s', { args: { productName } } ) }</h1>
+				<FormSectionHeading>{ this.translate( 'Remove %(productName)s', { args: { productName } } ) }</FormSectionHeading>
+				{ config.isEnabled( 'upgrades/removal-survey' ) ? this.renderSurvey() : null }
 				{ this.renderDialogText() }
 			</Dialog>
 		);
@@ -169,11 +206,94 @@ const RemovePurchase = React.createClass( {
 					}
 					{ ' ' }
 					{ this.translate( 'You will not be able to reuse it again without purchasing a new subscription.', {
-						comment: "'it' refers to a product purchased by a user"
+						context: 'Removal confirmation on Manage Purchase page',
+						comment: '"it" refers to a product purchased by a user'
 					} ) }
 				</p>
 
 				{ includedDomainText }
+			</div>
+		);
+	},
+
+	renderSurvey() {
+		let reasons = {},
+			ordered_reasons = [];
+
+		reasons.no_use = (
+			<FormLabel key="no_use">
+				<FormRadio
+					name="no_use"
+					value="no_use"
+					checked={ 'no_use' === this.state.checkedRadio }
+					onChange={ this.handleRadio } />
+				<span>{ this.translate( 'I no longer use this upgrade.' ) }</span>
+			</FormLabel>
+		);
+
+		reasons.not_sure_includes = (
+			<FormLabel key="not_sure_includes">
+				<FormRadio
+					name="not_sure_includes"
+					value="not_sure_includes"
+					checked={ 'not_sure_includes' === this.state.checkedRadio }
+					onChange={ this.handleRadio } />
+				<span>{ this.translate( 'I\'m not sure what this upgrade includes.' ) }</span>
+			</FormLabel>
+		);
+
+		reasons.different_expected = (
+			<FormLabel key="different_expected">
+				<FormRadio
+					name="different_expected"
+					value="different_expected"
+					checked={ 'different_expected' === this.state.checkedRadio }
+					onChange={ this.handleRadio } />
+				<span>{ this.translate( 'This upgrade doesn\'t work the way I thought it would.' ) }</span>
+			</FormLabel>
+		);
+
+		reasons.want_delete = (
+			<FormLabel key="want_delete">
+				<FormRadio
+					name="want_delete"
+					value="want_delete"
+					checked={ 'want_delete' === this.state.checkedRadio }
+					onChange={ this.handleRadio } />
+				<span>{ this.translate( 'I want to delete my site.' ) }</span>
+			</FormLabel>
+		);
+
+		reasons.another_reason = (
+			<FormLabel key="another_reason">
+				<FormRadio
+					name="another_reason"
+					value="another_reason"
+					checked={ 'another_reason' === this.state.checkedRadio }
+					onChange={ this.handleRadio } />
+				<span>{ this.translate( 'Another reason…' ) }</span>
+				<FormTextInput
+					name="another_reason_input"
+					id="another_reason_input"
+					disabled={ ! this.state.isAnotherReason }
+				/>
+			</FormLabel>
+		);
+
+		for ( let i in this.state.reasonOrder ) {
+			ordered_reasons.push( reasons[this.state.reasonOrder[i]] );
+		}
+
+		return (
+			<div>
+				<FormFieldset>
+					<FormLegend>{ this.translate( 'Please let us know why you are canceling:' ) }</FormLegend>
+					{ ordered_reasons }
+					<FormLabel>
+						{ this.translate( 'What could we do to improve? (optional)' ) }
+						<FormTextarea name="improvement_input" id="improvement_input" />
+					</FormLabel>
+				</FormFieldset>
 			</div>
 		);
 	},
@@ -184,7 +304,6 @@ const RemovePurchase = React.createClass( {
 		}
 
 		const purchase = getPurchase( this.props );
-
 		if ( ! isRemovable( purchase ) ) {
 			return null;
 		}
