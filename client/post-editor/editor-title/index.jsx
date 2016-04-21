692ecdafa7cb3e2b70d6836ed0f66ee73dee51de
Andrew Duthie <andrew@andrewduthie.com>
2016-04-21 15:53:35 -0400

    Editor: Set selection range after timeout
    Otherwise, IE11 sets selection to end of field

--- client/post-editor/editor-title/index.jsx
+++ client/post-editor/editor-title/index.jsx
@@ -56,10 +56,24 @@ const EditorTitle = React.createClass( {
 
 		// If value updated via paste, restore caret location
 		if ( this.selectionStart ) {
-			const input = ReactDom.findDOMNode( this.refs.titleInput );
-			input.setSelectionRange( this.selectionStart, this.selectionStart );
-			delete this.selectionStart;
+			this.setSelectionTimeout = setTimeout( this.setSelection, 0 );
 		}
+	},
+
+	componentWillUnmount() {
+		clearTimeout( this.setSelectionTimeout );
+		delete this.setSelectionTimeout;
+		delete this.selectionStart;
+	},
+
+	setSelection() {
+		if ( ! this.selectionStart ) {
+			return;
+		}
+
+		const input = ReactDom.findDOMNode( this.refs.titleInput );
+		input.setSelectionRange( this.selectionStart, this.selectionStart );
+		delete this.selectionStart;
 	},
 
 	setTitle( title ) {
@@ -111,8 +125,11 @@ const EditorTitle = React.createClass( {
 		valueChars.splice( selectionStart, selectionEnd - selectionStart, text );
 		const newTitle = valueChars.join( '' );
 
-		this.setTitle( newTitle );
+		// To preserve caret location, we track intended selection point to be
+		// restored after next render pass
 		this.selectionStart = selectionStart + text.length;
+
+		this.setTitle( newTitle );
 	},
 
 	preventEnterKeyPress( event ) {
