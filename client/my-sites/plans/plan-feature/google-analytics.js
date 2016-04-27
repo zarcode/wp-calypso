3526adee8448dace6d6bda8a260c6f06795fc09f
Bernhard Reiter <ockham@raz.or.at>
2016-04-27 23:08:11 +0200

    s/fill-rule/fillRule

--- client/my-sites/plans/plan-feature/google-analytics.js
+++ client/my-sites/plans/plan-feature/google-analytics.js
@@ -122,7 +122,7 @@ export default function( context ) {
 				<div className="plan-feature__logo">
 					<svg width="84" height="84" viewBox="0 0 84 84" xmlns="http://www.w3.org/2000/svg" style="overflow: visible;">
 						<title>icon - google analytics</title>
-						<g fill="none" fill-rule="evenodd">
+						<g fill="none" fillRule="evenodd">
 							<rect fill-opacity=".27" fill="#C8D7E1" x="4" y="4" width="80" height="80" rx="6"/>
 							<rect fill="#FFF" width="80" height="80" rx="6"/>
 							<path d="M45.16 49.896a8 8 0 1 1-14.137 4.477l-11.036-6.117A7.966 7.966 0 0 1 15 50c-.57 0-1.124-.06-1.66-.172L0 62.71v11.293A5.997 5.997 0 0 0 5.997 80h68.006A5.997 5.997 0 0 0 80 74.003V33.75l-12.512-8.127A7.963 7.963 0 0 1 63 27c-.76 0-1.495-.106-2.19-.304l-15.65 23.2z" stroke="#F05824" stroke-width="2" fill-opacity=".62" fill="#F05824"/><path d="M39.847 47.044A7.97 7.97 0 0 0 33.707 49l-10.773-5.97A8 8 0 1 0 8.292 46.36L0 54.37V5.996A5.997 5.997 0 0 1 5.997 0h68.006A5.997 5.997 0 0 1 80 5.997v20.597l-9.168-5.954a8 8 0 1 0-14.764 2.356l-16.22 24.048z" stroke="#F79A1F" stroke-width="2" fill-opacity=".68" fill="#F79A1F"/>
@@ -141,7 +141,7 @@ export default function( context ) {
 							<path id="a" d="M0 0h720v168H0z"/>
 							<path id="c" d="M3.958 95.457l64.13 10.647 64.447-30.737 63.947 15.8 62.526-45.334 64.804-26.5 64.646 12.724 63.263 35.09 64.463-21.06L580.333.87l64.178 18.397 63.95 52.7 64.12 23.122v50.48H.48"/>
 						</defs>
-						<g fill="none" fill-rule="evenodd">
+						<g fill="none" fillRule="evenodd">
 							<mask id="b" fill="#fff">
 								<use xlinkHref="#a"/>
 							</mask>
